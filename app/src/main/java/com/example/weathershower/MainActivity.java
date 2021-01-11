47caf59de1a3bfdad3db877b3760133713d0e48d
Lazar Antonijevic <lakyspider@gmail.com>
2021-01-11 23:57:38 +0100

    Finished app

--- app/src/main/java/com/example/weathershower/MainActivity.java
+++ app/src/main/java/com/example/weathershower/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.weathershower;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.input.InputManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -13,8 +20,12 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextCity;
+    TextView showCityTextView;
 
     public class DownloadData extends AsyncTask<String, Void, String> {
 
@@ -42,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
 
             } catch (Exception e) {
                 e.printStackTrace();
+
                 return null;
             }
         }
@@ -59,15 +71,44 @@ public class MainActivity extends AppCompatActivity {
 
                 JSONArray array = new JSONArray(weatherInfo);
 
+                String message = "";
+
                 for (int i=0; i < array.length(); i++) {
                     JSONObject jsonPartial = array.getJSONObject(i);
 
-                    Log.i("main", jsonPartial.getString("main"));
-                    Log.i("description", jsonPartial.getString("description"));
+                    String main = jsonPartial.getString("main");
+                    String description = jsonPartial.getString("description");
+
+                    if(!main.equals("") && !description.equals("")) {
+                        message += main + ": " + description + "\r\n";
+                    }
+                }
+
+                if(!message.equals("")){
+                    showCityTextView.setText(message);
+                } else {
+                    Toast.makeText(getApplicationContext(), "City doesn't exist. Please try again.", Toast.LENGTH_SHORT).show();
                 }
             } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "City doesn't exist. Please try again.", Toast.LENGTH_SHORT).show();
+
                 e.printStackTrace();
             }
+        }
+    }
+
+    public void getWeather(View view) {
+
+        try {
+            DownloadData dlData = new DownloadData();
+            String cityName = URLEncoder.encode(editTextCity.getText().toString(), "UTF-8");
+            dlData.execute("https://api.openweathermap.org/data/2.5/weather?q=" + cityName +"&appid=508eb656af8872b3cb9a54af0792283c");
+
+            InputMethodManager manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            manager.hideSoftInputFromWindow(showCityTextView.getWindowToken(), 0);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "City doesn't exist. Please try again.", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
         }
     }
 
@@ -76,7 +117,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DownloadData dlData = new DownloadData();
-        dlData.execute("https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=508eb656af8872b3cb9a54af0792283c");
+        editTextCity = findViewById(R.id.editTextCity);
+        showCityTextView = findViewById(R.id.showCityTextView);
+
     }
 }
