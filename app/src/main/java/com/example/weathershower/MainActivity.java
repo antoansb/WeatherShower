5dda007983545b9f558acf25157595f875f83137
Lazar Antonijevic <lakyspider@gmail.com>
2021-01-11 20:51:12 +0100

    URL/API starting point

--- app/src/main/java/com/example/weathershower/MainActivity.java
+++ app/src/main/java/com/example/weathershower/MainActivity.java
@@ -2,13 +2,61 @@ package com.example.weathershower;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    public class DownloadData extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnect = null;
+
+            try {
+
+                url = new URL(urls[0]);
+                urlConnect = (HttpURLConnection) url.openConnection();
+                InputStream input = urlConnect.getInputStream();
+                InputStreamReader reader = new InputStreamReader(input);
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.i("JSON", s);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DownloadData dlData = new DownloadData();
+        dlData.execute("https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=508eb656af8872b3cb9a54af0792283c");
     }
 }
